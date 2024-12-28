a690725f4057be63aeef2b2b7d1fd1a166987a8e
yoshida0003 <ren050800@icloud.com>
2024-12-28 23:44:03 +0900

    websocketでリアルタイムに入退出できる機能の追加

--- app/src/app/shogi-game/[roomId]/page.tsx
+++ app/src/app/shogi-game/[roomId]/page.tsx
@@ -16,6 +16,8 @@ const ShogiGame = () => {
   const userId = searchParams.get("userId");
 
   useEffect(() => {
+    console.log(`自分のユーザーID: ${userId}`);
+
     const fetchRoomData = async () => {
       try {
         const response = await axios.get(
@@ -32,18 +34,36 @@ const ShogiGame = () => {
     socket.emit("join-room", roomId);
 
     socket.on("user-joined", (user) => {
-      setUsers((prevUsers) => [...prevUsers, user]);
+      setUsers((prevUsers) => [
+        ...prevUsers,
+        { id: user.userId, username: user.username },
+      ]);
+      console.log(`${user.username}さんが入室しました。`);
     });
 
     socket.on("user-left", ({ userId }) => {
-      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+      setUsers((prevUsers) => {
+        console.log(
+          "現在のユーザーリスト:",
+          JSON.stringify(prevUsers, null, 2)
+        );
+        const updatedUsers = prevUsers.filter((user) => user.id !== userId);
+        console.log(
+          "更新後のユーザーリスト:",
+          JSON.stringify(updatedUsers, null, 2)
+        );
+        return updatedUsers;
+      });
+
+      console.log(`ユーザーID: ${userId}さんが退出しました。`);
     });
+
 
     return () => {
       socket.off("user-joined");
       socket.off("user-left");
     };
-  }, [roomId]);
+  }, [roomId, userId]);
 
   const handleLeaveRoom = async () => {
     try {
@@ -64,7 +84,7 @@ const ShogiGame = () => {
       <h3 className="text-lg mb-4">参加者:</h3>
       <ul className="list-disc pl-5">
         {users.map((user) => (
-          <li key={user.id} className="mb-2">
+          <li key={`${user.id}-${user.username}`} className="mb-2">
             {user.username} {user.id === userId && "(あなた)"}
           </li>
         ))}

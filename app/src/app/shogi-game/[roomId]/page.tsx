d897c978140f47dbb54a92cabc3e5ca89d76dc26
yoshida0003 <ren050800@icloud.com>
2025-01-25 10:31:16 +0900

    修正

--- app/src/app/shogi-game/[roomId]/page.tsx
+++ app/src/app/shogi-game/[roomId]/page.tsx
@@ -5,7 +5,7 @@ import { useRouter, useParams, useSearchParams } from "next/navigation";
 import axios from "axios";
 import io from "socket.io-client";
 
-const socket = io("https://game.yospace.org/api", {
+const socket = io("https://game.yospace.org", {
   withCredentials: true,
   transports: ["websocket"],
 });

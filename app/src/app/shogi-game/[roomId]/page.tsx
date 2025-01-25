f2adf86748b99015efeef0ea694d0eef954fae93
yoshida0003 <ren050800@icloud.com>
2025-01-25 18:16:46 +0900

    wss://game.yospace.org/websocketに修正

--- app/src/app/shogi-game/[roomId]/page.tsx
+++ app/src/app/shogi-game/[roomId]/page.tsx
@@ -5,7 +5,7 @@ import { useRouter, useParams, useSearchParams } from "next/navigation";
 import axios from "axios";
 import io from "socket.io-client";
 
-const socket = io("https://game.yospace.org/api", {
+const socket = io("wss://game.yospace.org/websocket", {
   withCredentials: true,
   transports: ["websocket", "polling"],
 });

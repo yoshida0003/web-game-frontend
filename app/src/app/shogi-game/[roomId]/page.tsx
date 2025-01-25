1ce8ee48a982153a4960b8b5d503ec2b38bbe1ef
yoshida0003 <ren050800@icloud.com>
2025-01-25 16:12:34 +0900

    リクエストURLの変更

--- app/src/app/shogi-game/[roomId]/page.tsx
+++ app/src/app/shogi-game/[roomId]/page.tsx
@@ -5,7 +5,7 @@ import { useRouter, useParams, useSearchParams } from "next/navigation";
 import axios from "axios";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001/api", {
+const socket = io("https://game.yospace.org/api", {
   withCredentials: true,
   transports: ["websocket"],
   reconnection: true,
@@ -44,7 +44,7 @@ const ShogiGame = () => {
     const fetchRoomData = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:3001/api/room/${roomId}`
+          `https://game.yospace.org/api/room/${roomId}`
         );
         setUsers(response.data.users);
         const currentUser = response.data.users.find(
@@ -112,7 +112,7 @@ const ShogiGame = () => {
 
   const handleLeaveRoom = async () => {
     try {
-      await axios.post(`http://localhost:3001/leave-room`, {
+      await axios.post(`https://game.yospace.org/api/leave-room`, {
         roomId,
         userId,
       });

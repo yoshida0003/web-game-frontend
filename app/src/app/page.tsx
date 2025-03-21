0a9a600ec848d11c80f37b8160938b61b09f41c4
yoshida0003 <ren050800@icloud.com>
2025-03-21 15:29:20 +0900

    認証の追加

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
 		<div className="grid row-span-1">
       <p><Link href="/shogi">将棋ページ</Link></p>
 			<p><Link href="./ng-word">NGワードゲーム</Link></p>
+      <p><Link href="./Auth">認証</Link></p>
     </div>
   );
 }
